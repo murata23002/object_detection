d630e6811c812075b6cfcf8bd895b54b37452c7f
s23002@st.kic.ac.jp <s23002@st.kic.ac.jp>
2024-09-19 18:58:04 +0900

    fix make work directory and run sh

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,6 @@ RUN apt-get update && \
     apt-get install -y libusb-1.0-0 libusb-1.0-0-dev git
 
 RUN pip install tflite-model-maker pycocotools
+
+ARG WKDIR=/workdir
+WORKDIR ${WKDIR}
--- tensor_flow_lite_image_exec_bash.sh
+++ tensor_flow_lite_image_exec_bash.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --rm -v ${pwd}:/work -it tensor_flow_maker_version_213:latest bash
+docker run --gpus all --rm -v $(pwd):/workdir -it tensor_flow_maker_version_213:latest bash
