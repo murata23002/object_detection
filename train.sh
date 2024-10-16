ec2b4adebe0a6c1c861106a0921e36cbbbaf6c82
s23002@st.kic.ac.jp <s23002@st.kic.ac.jp>
2024-10-17 02:30:53 +0900

    処理の再編成　チェックポイントから再トレーニング可能に変更

--- train.sh
+++ train.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-DATE=$(date +%Y%m%d_%H%M%S)
+DATE=$(date +%Y%m%d_%H%M%S) 
 
-SCRIPT_PATH=$(realpath "$0")
-
-CHECKOUT_DIR="checkout/checkout_1_$DATE"
-mkdir -p "$CHECKOUT_DIR"
-cp "$SCRIPT_PATH" "$CHECKOUT_DIR"
-
-python train.py --train ./dataset/head_face_body_limb \
-    --test ./dataset/head_face_body_limb \
-    --val ./dataset/head_face_body_limb \
-    --batch 6 \
-    --epochs 100 \
+python train.py --train ./path/to/dataset \
+    --test ./path/to/dataset \
+    --val ./path/to/dataset \
+    --batch 4 \
+    --epochs 500 \
     --tfilteName head_face_body_limb_$DATE.tflite \
-    --checkout "$CHECKOUT_DIR"
+    --checkout output/checkout_1_$DATE
